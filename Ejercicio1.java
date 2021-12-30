63eeb170ed40e174708d1fbd64c1a67d1f5febdf
Cristina Hidalgo Cobo <87225960+Xhexdoit@users.noreply.github.com>
2021-12-30 18:24:29 +0100

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,18 @@
+package Ejercicio1;
+
+public class SenuaEjercicio1 {
+
+    public static void main(String[] args) {
+        //tipos de datos
+
+        int num = 0;
+        double numd = 5.22;
+        long numL = 30000L;
+        float numf = 3.5f;
+        boolean stop = true;
+        char myChar = 'c';
+        String myString = "Senua";
+
+    }
+
+}
